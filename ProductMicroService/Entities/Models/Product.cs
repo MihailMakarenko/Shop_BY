1cb231671daea4c72b47a94cb492fbc07e0bad8a
DESKTOP-HA6DQK0\Mihail <misha.makarenko2004@yandex.ru>
2025-05-24 00:24:38 +0300

    Create ProductService, add user model

---
+++ ProductMicroService/Entities/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public string? CreatedByUserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdateAt { get; set; }
+    }
+}
---
+++ ProductMicroService/Repository/Configuration/ProductConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(200);
+
+            builder.Property(p => p.Description).IsRequired().HasMaxLength(1000);
+
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)").HasPrecision(18, 2);
+
+            builder.Property(p => p.IsAvailable).HasDefaultValue(true);
+
+            builder.Property(p => p.CreatedByUserId).IsRequired();
+
+            builder.Property(p => p.CreatedAt).HasDefaultValueSql("GETUTCDATE()").ValueGeneratedOnAdd();
+
+            builder.Property(p => p.UpdateAt).ValueGeneratedOnAddOrUpdate().HasDefaultValueSql("GETUTCDATE()");
+
+            builder.Property(p => p.CreatedByUserId).IsRequired();
+
+            builder.HasIndex(p => p.CreatedByUserId);
+
+
+        }
+
+     
+
+        //public ProductConfiguration(EntityTypeBuilder<Product> builder)
+        //{
+
+
+        //}
+    }
+}
